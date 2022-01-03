bd89c63e7336a10e8989fdf5e33408ed29874fb5
ksh-fthr <ksh.fthr0806@gmail.com>
2022-01-03 23:15:08 +0900

    メニューをタブ化.

--- webapp/app/src/Routing.js
+++ webapp/app/src/Routing.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+import './Routing.css';
 import App from './component/App/App';
 import Hello from './component/Hello/Hello';
 
+const tabClassName = 'tab-item';
+const currentTabClassName = `${tabClassName} current`;
+
 const Routing = () => {
+
+  const currentTab = (event) => {
+    // TODO: React 的には `ref` を使うのが作法っぽいので後で見直す
+    const targetElements = document.getElementsByClassName(tabClassName);
+
+    // getElementsByClassName で取得したオブジェクトには forEach が実装されていないので
+    // Array.from で変換する
+    Array.from(targetElements).forEach((element) => {
+      element.className = tabClassName;
+    });
+
+    event.target.parentElement.className = currentTabClassName;
+  };
+
   return (
     <BrowserRouter>
-      <div>
-        <ul>
-          <li><Link to='/'>React</Link></li>
-          <li><Link to='/api/hello'>Hello</Link></li>
+      <div className='tab-area-base'>
+        <ul className='tab-menu-base'>
+          <li className={currentTabClassName} onClick={currentTab}><Link className='li-link' to='/'>React</Link></li>
+          <li className={tabClassName} onClick={currentTab}><Link className='li-link' to='/api/hello'>Hello</Link></li>
         </ul>
         <Routes>
           <Route path='/' element={<App />} />
