4479f4fd39efb8517c24c5cc0acb5b2cd1a4acfc
ksh-fthr <ksh.fthr0806@gmail.com>
2022-01-23 12:34:33 +0900

    contetns api を testapi に変更.

--- restapi/api/router/routing.go
+++ restapi/api/router/routing.go
@@ -1,8 +1,8 @@
 package router
 
 import (
-	"restapi/contents"
 	"restapi/hello"
+	"restapi/testapi"
 
 	"github.com/labstack/echo/v4"
 )
@@ -16,9 +16,9 @@ func Routing(e *echo.Echo) {
 	e.GET("/hello", hello.Hello)
 
 	// Contents API
-	e.GET("/contents/all", contents.All)
-	e.GET("/contents/:id", contents.Content)
-	e.POST("/contents", contents.Register)
-	e.PUT("/contents/:id", contents.Update)
-	e.DELETE("/contents/:id", contents.Delete)
+	e.GET("/testapi/all", testapi.All)
+	e.GET("/testapi/:id", testapi.Content)
+	e.POST("/testapi", testapi.Register)
+	e.PUT("/testapi/:id", testapi.Update)
+	e.DELETE("/testapi/:id", testapi.Delete)
 }
