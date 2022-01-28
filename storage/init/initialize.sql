9c28abfa31e85166cdc41825ffb83576b51f8c8c
ksh-fthr <ksh.fthr0806@gmail.com>
2022-01-28 20:55:52 +0900

    作成される DB のテーブル定義を変更.

--- storage/init/initialize.sql
+++ storage/init/initialize.sql
@@ -3,10 +3,12 @@ create database mydb;
 
 use mydb;
 
-drop table if exists mydata;
-create table if not exists mydata (
+drop table if exists contents;
+create table if not exists contents (
 	id serial,
-    data text not null,
+    title varchar(255) not null,
+    contents text not null,
+    remarks varchar(1024) not null,
     created_at datetime DEFAULT NULL,
     updated_at datetime DEFAULT NULL,
     primary key(id)
