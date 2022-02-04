967b6f75417294578e3a60ff3de5ed1d570e6cdf
ksh-fthr <ksh.fthr0806@gmail.com>
2022-02-04 23:19:34 +0900

    DB 接続処理を別ファイルに切り出した.

--- restapi/api/modules/contents/contents.go
+++ restapi/api/modules/contents/contents.go
@@ -1,30 +1,24 @@
 package contents
 
 import (
-	"database/sql"
 	"fmt"
 	"net/http"
 
-	_ "github.com/go-sql-driver/mysql"
+	"restapi/service/dbconnect"
+
 	"github.com/labstack/echo/v4"
 )
 
 func TryConnect(c echo.Context) error {
-	fmt.Println("exec db::connect.")
+	fmt.Println("exec contents::TryeConnect.")
 
-	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
-	dbconf := "mysql:mysqladmin@tcp(172.30.10.100:3306)/mydb?charset=utf8mb4"
-
-	db, err := sql.Open("mysql", dbconf)
+	db := dbconnect.Connect()
 
 	// 接続が終了したらクローズする
 	defer db.Close()
 
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	err = db.Ping()
+	// 疎通確認
+	err := db.Ping()
 
 	if err != nil {
 		fmt.Println("データベース接続失敗")
---
+++ restapi/api/service/dbconnect/dbconnect.go
@@ -0,0 +1,23 @@
+package dbconnect
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect() *sql.DB {
+	fmt.Println("exec db::connect.")
+
+	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
+	dbconf := "mysql:mysqladmin@tcp(172.30.10.100:3306)/mydb?charset=utf8mb4"
+
+	db, err := sql.Open("mysql", dbconf)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return db
+}
