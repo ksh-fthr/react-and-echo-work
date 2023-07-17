5bf6392eed610091b531f83b9fc1551e5ee6af3c
ksh-fthr <ksh.fthr0806@gmail.com>
2023-07-17 16:59:53 +0900

    コンテンツの各モードにパンくずリストの原型を用意した.

--- webapp/app/src/component/Contents/Create/CreateContents.js
+++ webapp/app/src/component/Contents/Create/CreateContents.js
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const CreateContents = () => {
 
   return (
     <div className="content-index">
+      <h2>Contents</h2>
+      <div>
+        コンテンツ一覧
+      </div>
+      <ul>
+        <li><Link to='/contents'>コンテンツ一覧</Link></li>
+        <li><Link to='/contents/view'>閲覧</Link></li>
+        <li><Link to='/contents/edit'>編集</Link></li>
+        <li><Link to='/contents/delete'>削除</Link></li>
+      </ul>
       <div>
         Create Contents
       </div>
--- webapp/app/src/component/Contents/Delete/DeleteContents.js
+++ webapp/app/src/component/Contents/Delete/DeleteContents.js
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const DeleteContents = () => {
 
   return (
     <div className="content-index">
+      <h2>Contents</h2>
+      <div>
+        コンテンツ一覧
+      </div>
+      <ul>
+        <li><Link to='/contents'>コンテンツ一覧</Link></li>
+        <li><Link to='/contents/create'>新規作成</Link></li>
+        <li><Link to='/contents/view'>閲覧</Link></li>
+        <li><Link to='/contents/edit'>編集</Link></li>
+      </ul>
       <div>
         Delete Contents
       </div>
