d9a4bad12e4a6f64617591524c01f37e6f05efd9
ksh-fthr <ksh.fthr0806@gmail.com>
2023-07-30 15:09:43 +0900

    コンテンツ一覧に閲覧・編集・削除へのページリンクを設定.それに合わせて sidebar の閲覧・編集・削除のリンクを削除

--- webapp/app/src/component/Contents/Delete/DeleteContents.js
+++ webapp/app/src/component/Contents/Delete/DeleteContents.js
@@ -16,9 +16,6 @@ const DeleteContents = () => {
           <li><Link to='/contents'>コンテンツTOP</Link></li>
           <li><Link to='/contents/list'>一覧</Link></li>
           <li><Link to='/contents/create'>新規作成</Link></li>
-          <li><Link to='/contents/view'>閲覧</Link></li>
-          <li><Link to='/contents/edit'>編集</Link></li>
-          <li className="disable-link">削除</li>
         </ul>
       </div>
       <div className="main">
--- webapp/app/src/component/Contents/Edit/EditContents.js
+++ webapp/app/src/component/Contents/Edit/EditContents.js
@@ -16,9 +16,6 @@ const EditContents = () => {
           <li><Link to='/contents'>コンテンツTOP</Link></li>
           <li><Link to='/contents/list'>一覧</Link></li>
           <li><Link to='/contents/create'>新規作成</Link></li>
-          <li><Link to='/contents/view'>閲覧</Link></li>
-          <li className="disable-link">編集</li>
-          <li><Link to='/contents/delete'>削除</Link></li>
         </ul>
       </div>
       <div className="main">
