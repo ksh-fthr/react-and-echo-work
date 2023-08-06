207aa0563d45325d0c1bed08a502e29473a1faa7
ksh-fthr <ksh.fthr0806@gmail.com>
2023-08-06 16:48:51 +0900

    TOP ページで扱うモジュールを Routing モジュールに変更.

--- webapp/app/src/index.js
+++ webapp/app/src/index.js
@@ -1,7 +1,7 @@
 import './css/index.css'
 import reportWebVitals from './reportWebVitals'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import Routing from './Routing'
 
 // 下記が発生していたので対応.
 // ReactDOM.render is deprecated since React 18.0.0, use createRoot instead, see https://reactjs.org/link/switch-to-createroot
@@ -9,7 +9,7 @@ import App from './App'
 // See: https://zenn.dev/kohski/articles/create-react-app-error-v18
 const container = document.getElementById('root')
 const root = createRoot(container)
-root.render(<App />)
+root.render(<Routing />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
