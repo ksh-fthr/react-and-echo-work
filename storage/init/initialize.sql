65e8052617751e04f559b79d01a6e78a24f4c3e1
ksh-fthr <ksh.fthr0806@gmail.com>
2024-03-17 22:53:07 +0900

    update DB Table Definition.

--- storage/init/initialize.sql
+++ storage/init/initialize.sql
@@ -3,15 +3,29 @@ create database mydb;
 
 use mydb;
 
-drop table if exists contents;
+drop table if exists Contents;
 create table if not exists contents (
-    id serial,
+    id serial primary key,
     title varchar(255) not null,
-    contents text not null,
-    remarks varchar(1024) not null,
+    author varchar(255),
+    summary text,
+    deleted boolean not null,
     created_at datetime DEFAULT NULL,
     updated_at datetime DEFAULT NULL,
-    primary key(id)
+);
+
+drop table if exists Article;
+create table if not exists Article (
+    id serial primary key,
+    content_id int not null,
+    subtitle varchar(10245) not null,
+    body text not null,
+    remarks text,
+    deleted boolean not null,
+    created_at datetime DEFAULT NULL,
+    updated_at datetime DEFAULT NULL,
+
+    foreign key (content_id) references contents(id)
 );
 
 grant all privileges on mydb.* to `mysql`@'%';
