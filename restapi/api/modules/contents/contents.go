efa266d1992d6b29bed5c5120df4d223ecf047b2
ksh-fthr <ksh.fthr0806@gmail.com>
2024-04-27 22:59:35 +0900

    formatter による整形.

--- restapi/api/modules/contents/contents.go
+++ restapi/api/modules/contents/contents.go
@@ -1,13 +1,17 @@
 package contents
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net/http"
 
+	"restapi/orm/gen/query"
 	"restapi/service/dbconnect"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func TryConnect(c echo.Context) error {
@@ -31,30 +35,23 @@ func TryConnect(c echo.Context) error {
 func GetAllContents(c echo.Context) error {
 	log.Println("exec contents::GetAllContents.")
 
-	var (
-		id       int
-		title    string
-		contents string
-		remarks  string
-	)
+	// db := dbconnect.Connect()
+	// defer dbconnect.DisConnect(db)
 
-	db := dbconnect.Connect()
-	defer dbconnect.DisConnect(db)
+	dbconf := "mysql:mysqladmin@tcp(172.30.10.100:3306)/mydb?charset=utf8mb4"
+	gormdb, _ := gorm.Open(mysql.Open(dbconf))
 
-	rows, err := db.Query("select id, title, contents, remarks from contents")
-	checkErr(err)
-	defer rows.Close()
+	qu := query.Use(gormdb)
+	ctx := context.Background()
+	contentsQuery := qu.Content
 
-	log.Println("Total count:", checkCount(rows))
-	for rows.Next() {
-		err := rows.Scan(&id, &title, &contents, &remarks)
-		checkErr(err)
-		log.Println(id, title, contents, remarks)
+	_, err := contentsQuery.WithContext(ctx).Order(contentsQuery.ID.Desc()).Find()
+	if err != nil {
+		log.Println("クエリ実行失敗")
+		return c.String(http.StatusOK, "ERROR: Execute Query.\n")
 	}
-	err = rows.Err()
-	checkErr(err)
 
-	return c.String(http.StatusOK, "Success: DB Exec Query.\n")
+	return c.String(http.StatusOK, "Success: DB Execute Query.\n")
 }
 
 func checkCount(rows *sql.Rows) (count int) {
