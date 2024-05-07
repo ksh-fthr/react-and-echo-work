2cae57f79f2f7baf552b8675cd24c054bef26e9a
ksh-fthr <ksh.fthr0806@gmail.com>
2024-05-07 09:23:13 +0900

    ORM による CRUD の C を実装.

--- restapi/api/modules/contents/contents.go
+++ restapi/api/modules/contents/contents.go
@@ -2,15 +2,23 @@ package contents
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 
+	"restapi/orm/gen/model"
 	"restapi/orm/gen/query"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+type ContentData struct {
+	Title   string `json:"title"`
+	Author  string `json:"author"`
+	Summary string `json:"summary"`
+}
 
 func GetAllContents(c echo.Context) error {
 	log.Println("exec contents::GetAllContents.")
@@ -37,6 +45,38 @@ func GetContents(c echo.Context) error {
 
 func RegisterContents(c echo.Context) error {
 	log.Println("exec contents::RegisterContent.")
+	data := new(ContentData)
+
+	if err := c.Bind(data); err != nil {
+		return err
+	}
+
+	dbconf := "mysql:mysqladmin@tcp(172.30.10.100:3306)/mydb?charset=utf8mb4"
+	gormdb, _ := gorm.Open(mysql.Open(dbconf))
+
+	qu := query.Use(gormdb)
+	ctx := context.Background()
+	contentsQuery := qu.Content
+
+	contents := &model.Content{
+		Title:   data.Title,
+		Author:  data.Author,
+		Summary: data.Summary,
+	}
+
+	err := qu.Transaction(func(tx *query.Query) error {
+		if err := contentsQuery.WithContext(ctx).Create(contents); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusOK, "Error: DB Register Content.\n")
+	}
+
 	return c.String(http.StatusOK, "Success: DB Register Content.\n")
 }
 
