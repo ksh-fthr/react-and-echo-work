5e00bface840ef5d1d4cf641e6d71b3bc0c37559
ksh-fthr <ksh.fthr0806@gmail.com>
2024-05-12 13:38:06 +0900

    DB接続用の Util を GORM 用に修正.

--- restapi/api/service/dbconnect/dbconnect.go
+++ restapi/api/service/dbconnect/dbconnect.go
@@ -1,28 +1,25 @@
 package dbconnect
 
 import (
-	"database/sql"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func Connect() *sql.DB {
+func Connect() *gorm.DB {
 	log.Println("exec dbconnect::Connect.")
 
-	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
-	dbconf := "mysql:mysqladmin@tcp(172.30.10.100:3306)/mydb?charset=utf8mb4"
-
-	db, err := sql.Open("mysql", dbconf)
-
+	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]&parseTime=true
+	//
+	// 注) TIMESTAMP型 のカラムを返却するために parseTime=true をつける
+	// https://stackoverflow.com/questions/45040319/unsupported-scan-storing-driver-value-type-uint8-into-type-time-time
+	dbconf := "mysql:mysqladmin@tcp(172.30.10.100:3306)/mydb?charset=utf8mb4&parseTime=true"
+	gormdb, err := gorm.Open(mysql.Open(dbconf))
 	if err != nil {
 		log.Println(err.Error())
 	}
 
-	return db
-}
-
-func DisConnect(db *sql.DB) {
-	log.Println("exec dbconnect::DisConnect.")
-	db.Close()
+	return gormdb
 }
