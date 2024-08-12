96e779b814ee6cbd296f7ad51856b72f94977ab4
ksh-fthr <ksh.fthr0806@gmail.com>
2024-08-12 12:31:51 +0900

    記事削除画面に React Material-UI で削除時のアラートダイアログを実装.

--- webapp/app/src/component/PostingSite/Articles/Delete/DeleteArticle.js
+++ webapp/app/src/component/PostingSite/Articles/Delete/DeleteArticle.js
@@ -1,8 +1,39 @@
 import { Link, useParams } from 'react-router-dom'
+import { useState } from 'react'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
 
 const DeleteArticle = () => {
   // これだけで URL パラメータから値を取得できる
   const params = useParams()
+  const [open, setOpen] = useState(false)
+
+  /**
+   * ダイアログオープン
+   * ------------------------------------------
+   * material-ui での alert-dialog の実装
+   * https://mui.com/material-ui/react-dialog/
+   */
+  const handleClickOpen = () => {
+    setOpen(true)
+    deleteArticle()
+  }
+
+  /**
+   * ダイアログクローズ
+   * ------------------------------------------
+   * material-ui での alert-dialog の実装
+   * https://mui.com/material-ui/react-dialog/
+   */
+  const handleClose = () => {
+    setOpen(false)
+  }
 
   const deleteArticle = () => {
     console.dir({
@@ -41,13 +72,52 @@ const DeleteArticle = () => {
           <h3>記事削除</h3>
         </div>
         <div className="article-body">
-          ID: {params.articleId}, サブタイトル: {params.subtitle} を削除します
+          <Box
+            sx={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              '& > :not(style)': {
+                m: 0,
+                width: '100%',
+                height: 128
+              }
+            }}
+          >
+            <Paper
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              ID: {params.articleId}, サブタイトル: {params.subtitle} を削除します
+            </Paper>
+          </Box>
         </div>
         <div className="article-footer">
-          <button type="button" onClick={deleteArticle}>
+          <Button type="button" variant="outlined" onClick={handleClickOpen}>
             送信
-          </button>
+          </Button>
         </div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {'指定したコンテンツを削除します'}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              削除すると元には戻せません。よろしいですか？
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>いいえ。削除しません</Button>
+            <Button onClick={handleClose} autoFocus>はい。削除します</Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </div>
   )
